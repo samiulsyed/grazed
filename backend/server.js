5abb5335629e5d1395eafc4b176188f278ebf9ce
samiulsyed <sam@devgenius.com.au>
2021-10-07 23:28:24 +1100

    backend server added

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
